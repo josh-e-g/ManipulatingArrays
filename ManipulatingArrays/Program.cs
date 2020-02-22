609d961dfb4dffd6253c8e655f4d36f9b0ab4979
Josh Gaston <wje.gaston@gmail.com>
2020-02-22 11:53:27 -0800

    Changed string outputs to arrays

--- ManipulatingArrays/Program.cs
+++ ManipulatingArrays/Program.cs
@@ -10,32 +10,39 @@ namespace ManipulatingArrays
 			int[] arrayA = { 0, 2, 4, 6, 8, 10 };
 			int[] arrayB = { 1, 3, 5, 7, 9 };
 			int[] arrayC = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
-			Console.WriteLine($"\nArray A is: [{Print(arrayA)}]");
+			Console.WriteLine($"\nArray A is: {Print(arrayA)}");
 			Console.WriteLine($"\n\tThe average of Array A is {Average(arrayA)}.");
-			Console.WriteLine($"\tArray A reversed is [{Reverse(arrayA)}].");
-			Console.WriteLine($"\tArray A rotated two places to the left is [{Rotate("left", 2, arrayA)}].");
-			Console.WriteLine($"\tArray A sorted from least to greatest is [{Print(Sort(arrayA))}].");
+			Console.WriteLine($"\tArray A reversed is {Print(Reverse(arrayA))}.");
+			Console.WriteLine($"\tArray A rotated two places to the left is {Rotate("left", 2, arrayA)}.");
+			Console.WriteLine($"\tArray A sorted from least to greatest is {Print(Sort(arrayA))}.");
 
-			Console.WriteLine($"\nArray B is: [{Print(arrayB)}]");
+			Console.WriteLine($"\nArray B is: {Print(arrayB)}");
 			Console.WriteLine($"\n\tThe average of Array B is {Average(arrayB)}.");
-			Console.WriteLine($"\tArray B reversed is [{Reverse(arrayB)}].");
-			Console.WriteLine($"\tArray B rotated two places to the right is [{Rotate("right", 2, arrayB)}].");
-			Console.WriteLine($"\tArray B sorted from least to greatest is [{Print(Sort(arrayB))}].");
+			Console.WriteLine($"\tArray B reversed is {Print(Reverse(arrayB))}.");
+			Console.WriteLine($"\tArray B rotated two places to the right is {Rotate("right", 2, arrayB)}.");
+			Console.WriteLine($"\tArray B sorted from least to greatest is {Print(Sort(arrayB))}.");
 
-			Console.WriteLine($"\nArray C is: [{Print(arrayC)}]");
+			Console.WriteLine($"\nArray C is: {Print(arrayC)}");
 			Console.WriteLine($"\n\tThe average of Array C is {Average(arrayC)}.");
-			Console.WriteLine($"\tArray C reversed is [{Reverse(arrayC)}].");
-			Console.WriteLine($"\tArray C rotated four places to the left is [{Rotate("left", 4, arrayC)}].");
-			Console.WriteLine($"\tArray C sorted from least to greatest is [{Print(Sort(arrayC))}].");
+			Console.WriteLine($"\tArray C reversed is {Print(Reverse(arrayC))}.");
+			Console.WriteLine($"\tArray C rotated four places to the left is {Rotate("left", 4, arrayC)}.");
+			Console.WriteLine($"\tArray C sorted from least to greatest is {Print(Sort(arrayC))}.");
 		}
 		public static string Print(int[] array)
 		{
-			string printed = "";
-			foreach (var i in array)
+			string result = "[";
+			for (int i = 0; i < array.Length; i++)
 			{
-				printed += i + ", ";
+				if (i < array.Length - 1)
+				{
+					result += $"{array[i]},";
+				}
+				else
+				{
+					result += $"{array[i]}]";
+				}
 			}
-			return printed;
+			return result;
 		}
 		public static double Average(int[] array)
 		{
@@ -47,43 +54,53 @@ namespace ManipulatingArrays
 			average = (double)sum / (double)array.Length;
 			return average;
 		}
-		public static string Reverse(int[] array)
+		public static int[] Reverse(int[] array)
 		{
-			string reversed = "";
+			int[] reversed = new int[array.Length];
+			int count = 0;
 
-			for (int i = array.Length - 1; i >= 0; i--) reversed += array[i] + ", ";
-			
+			for (int i = array.Length - 1; i >= 0; i--)
+			{
+				reversed[count] = array[i];
+				count++;
+			}
 			return reversed;
 		}
 		public static string Rotate(string direction, int number, int[] array)
 		{
-			string front = "";
-			string back = "";
+			int[] result = new int[array.Length];
+			int indexBack = result.Length - number;
+			int indexFront = 0;
+			int indexNum = number;
 			if (direction == "left")
 			{
-				for (int i = 0; i < number; i++)
+				for (int i = 0; i < number; i++)//puts the first x num of elements of input arr into the back of the output arr
 				{
-					front += array[i] + ", ";
+					result[indexBack] = array[i];
+					indexBack++;
 				}
-				for (int i = number; i < array.Length; i++)
+				for (int i = number; i < array.Length; i++)//puts the remaining elements of input arr into front of output arr
 				{
-					back += array[i] + ", ";
+					result[indexFront] = array[i];
+					indexFront++;
 				}
+				
+				return Print(result);
 
-				return back + front;
 			}
 			else if (direction == "right")
 			{
-				for (int i = array.Length - number; i < array.Length; i++)
+				for (int i = array.Length - number; i < array.Length; i++)//puts the last x num of elements of input arr into front of output arr
 				{
-					back += array[i] + ", ";
+					result[indexFront] = array[i];
+					indexFront++;
 				}
-				for (int i = 0; i < array.Length - number; i++)
+				for (int i = 0; i < array.Length - number; i++)//puts the front of input array into the remaining spaces of output arr
 				{
-					front += array[i] + ", ";
+					result[indexNum] = array[i];
+					indexNum++;
 				}
-
-				return back + front;
+				return Print(result);
 			}
 			else
 			{
@@ -100,9 +117,9 @@ namespace ManipulatingArrays
 				{
 					if (array[i] > array[i + 1])
 					{
-						int p = array[i];
+						int temp = array[i];
 						array[i] = array[i + 1];
-						array[i + 1] = p;
+						array[i + 1] = temp;
 						count += 1;
 					}
 				}
